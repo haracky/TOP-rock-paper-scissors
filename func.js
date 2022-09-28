07d1dce8a2f8b3a0e87910e2f022aa25799714ac
Ondrej Vymola <ovymola+git@gmail.com>
2022-09-28 20:22:29 +0200

    getPlayerChoice validates and returns user input

--- func.js
+++ func.js
@@ -1,6 +1,29 @@
-const OPTIONS = ['Rock','Paper','Scissors'];
+const OPTIONS = ['rock','paper','scissors'];
+
 
 function getComputerChoice() {
-    option = Math.round(Math.random()*3);
-    console.log(OPTIONS[option]);
+    /// returns random of Rock Paper or Scissors
+    ///  console.log(OPTIONS[Math.round(Math.random()*3)]); 
+
+    return OPTIONS[Math.round(Math.random()*3)];
+}
+
+
+function getPlayerChoice() {
+    /// gets choice from player
+
+    let playerChoice = window.prompt("Rock or Paper or Scissors?");
+    
+    while (1==1){
+        
+        if ((playerChoice.toLowerCase() == 'rock') || (playerChoice.toLowerCase() == 'paper') || (playerChoice.toLowerCase() == 'scissors')) {
+            console.log(playerChoice);
+            return playerChoice.toLowerCase();
+        }
+
+        playerChoice = window.prompt("WRONG INPUT: Rock or Paper or Scissors?");
+    }
+   
+    
+
 }
